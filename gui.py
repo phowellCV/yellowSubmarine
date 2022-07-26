63291e6876ba748c880fe997bbc65b560aa040e1
JoelcJohnson <joelcj@comcast.net>
2022-07-25 20:31:54 -0500

    Add command window

--- gui.py
+++ gui.py
@@ -1,8 +1,24 @@
 from kivy.app import App
-from kivy.uix.button import Button
- 
-class TestApp(App):
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+
+class Terminal(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(Terminal, self).__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text="Page"))
+        self.commandLine = TextInput(multiline=False)
+        self.add_widget(self.commandLine)
+
+
+class MyApp(App):
+
     def build(self):
-        return Button(text= " Hello World ")
- 
-TestApp().run()
+        return Terminal()
+
+
+if __name__ == '__main__':
+    MyApp().run()
